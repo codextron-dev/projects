cca65e96389968df54f4733eb86f412b1c2b31c9
codextron <codextron@proton.me>
2026-08-22 20:01:08 +0545

    qr scanner

---
+++ qr-scanner/main.py
@@ -0,0 +1,10 @@
+import cv2
+
+// provide your image file here
+file = "image.png"
+
+img = cv2.imread(file)
+detector = cv2.QRCodeDetector()
+data, points, _ = detector.detectAndDecode(img)
+
+print("QR Code Data: ", data)
